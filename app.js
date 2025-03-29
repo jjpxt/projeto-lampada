c7516ada931d6b13f53f200dce4cf2908bfa504f
jjpxt <jotajunior.peixoto@gmail.com>
2025-03-28 22:11:47 -0300

    adicionando nova funcionalidade

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const turnOn = document.getElementById("turnOn");
 const turnOff = document.getElementById("turnOff");
 const lamp = document.getElementById("lamp");
 
+let turnedOnbyBtn = false;
+
 function isLampBroken() {
   return lamp.src.indexOf("quebrada") > -1;
 }
@@ -13,8 +15,22 @@ function lampOn() {
 }
 
 function lampOff() {
+  if (!isLampBroken() && !turnedOnbyBtn) {
+    lamp.src = "./img/desligada.jpg";
+  }
+}
+
+function buttonLampOn() {
+  if (!isLampBroken()) {
+    lamp.src = "./img/ligada.jpg";
+    turnedOnbyBtn = true;
+  }
+}
+
+function buttonLampOff() {
   if (!isLampBroken()) {
     lamp.src = "./img/desligada.jpg";
+    turnedOnbyBtn = false;
   }
 }
 
@@ -22,9 +38,8 @@ function brokenLamp() {
   lamp.src = "./img/quebrada.jpg";
 }
 
-turnOn.addEventListener("click", lampOn);
-turnOff.addEventListener("click", lampOff);
+turnOn.addEventListener("click", buttonLampOn);
+turnOff.addEventListener("click", buttonLampOff);
 lamp.addEventListener("mouseover", lampOn);
 lamp.addEventListener("mouseleave", lampOff);
-
 lamp.addEventListener("dblclick", brokenLamp);
